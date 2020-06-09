6d141eaf88786eb4e22addc9dfcd0b818e74b635
Shakti <shakti.opensourcedeveloper@gmail.com>
2020-06-09 10:45:09 +0530

    server.js fixes

--- server.js
+++ server.js
@@ -56,7 +56,3 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.get('/service-worker.js', (req,res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-})
